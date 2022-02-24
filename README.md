91fc9236c8a319865e882968e1855c3faa6f2a45
Scott Moore <scott.moore@dfsupplyinc.com>
2022-02-24 15:59:42 -0500

    initial working commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DFS_xTupleBuildTools
-xTuple Build Tools
+Compiles xTuple 6.x in a docker container for multiple platforms
+
+Tested and built for RHEL 8.x
+
+## You must have git setup already with SSH key (as this uses private REPOs)
+
+Currently setup to clone from private DFS repositories. To use your own REPO, change the shell script(s) as required.
+
+Compile for native (x64_linux):
+```
+cd /root/
+git clone https://github.com/DFSupply/DFS_xTupleBuildTools.git
+cd DFS_xTupleBuildTools
+chmod +x compile-linux.sh
+./compile-linux.sh
+```
---
+++ compile-linux.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# xTuple Build Tools
+#
+# DF Supply, Inc.
+# 02/24/2022
+
+# DO NOT USE IN PRODUCTION WITHOUT PRIOR TESTING
+#
+# Requires:
+# RHEL 8.x
+#
+# Built for RHEL 8 by Scott D Moore @ DF Supply - scott.moore@dfsupplyinc.com
+
+if [[ $(id -u) -ne 0 ]]
+	then
+		echo "Please run this script as root or sudo... Exit."
+		exit 1
+fi
+
+if grep -q -i "Red Hat Enterprise Linux release 8" /etc/redhat-release; then
+	echo "running RHEL 8.x"
+	OS_VER="RHEL8"
+else
+	echo "Unsupported OS. See README for tested distributions."
+	OS_VER="UNSUP"
+	exit 1
+fi
+
+echo "xTuple Compile Utility (linux x64)"
+echo "DF Supply, Inc."
+echo ""
+
+echo ""
+echo "Please confirm you wish to proceed with the build? (y/n)"
+read -r
+if [[ ! "$REPLY" =~ ^[Yy]$ ]]; then
+	echo "Cancelling..."
+	exit 1
+fi
+
+echo ""
+echo "Building Qt Environment..."
+
+subscription-manager repos --enable=codeready-builder-for-rhel-8-x86_64-rpms
+yum install podman -y
+git clone https://github.com/DFSupply/DFS_QtApplicationCompileEnvironment
+cd DFS_QtApplicationCompileEnvironment
+podman build -f DockerFile -t qt-build-env:latest
+podman run --name qt-build-xtuple -it qt-build-env:latest
+
+echo ""
+echo "Qt Environment Running..."
+echo "Building xTuple Client Now..."
+cd ..
+git clone https://github.com/DFSupply/qt-client
+cd qt-client
+git submodule update --init --recursive
+cd ..
+
+
+echo ""
+echo "Finished!"
+echo ""
